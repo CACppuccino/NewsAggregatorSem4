0e6d445fa40fd68f8afed7def456f5f6ac67c7ee
Ziyang Liu <u6210090@anu.edu.au>
2020-09-02 03:55:16 +1000

    added a displayable frontend

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for, render_template, send_from_directory
+from flask_cors import CORS, cross_origin
 from minitask.simple_search import simple_match_search
 from elasticsearch import Elasticsearch
 
@@ -6,9 +7,23 @@ from elasticsearch import Elasticsearch
 INDEX_NAME = 'news'
 ES = Elasticsearch([{'host' : 'localhost', 'port': 9200}])
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
+@app.route('/')
+def index_page():
+    return render_template('index.html')
+
+@app.route('/css/<path:filename>')
+def send_css(filename):
+    return send_from_directory('static/css', filename)
+
+@app.route('/js/<path:filename>')
+def send_js(filename):
+    return send_from_directory('static/js', filename)
 
 @app.route('/search')
+@cross_origin()
 def search():
     query = request.args.get('query', None)
     if query:
